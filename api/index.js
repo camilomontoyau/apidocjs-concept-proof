292af94915a65925155fb035a113412894faf65a
Camilo <camilomontoya2@gmail.com>
2016-04-11 16:20:07 -0500

    getOne, put, delete end points on apidoc notatios

--- api/index.js
+++ api/index.js
@@ -42,6 +42,48 @@ var Task = require('./models/task');
  */
 
 /**
+ * @api {get} /task/:id Request One Task information
+ * @apiSampleRequest /api/task/570bfd4ffcc31d60194e38bd
+ * @apiName GetOneTask
+ * @apiGroup Task
+ *
+ * @apiSuccess {String} _id ID of the task.
+ * @apiSuccess {Date} updatedAt updated date
+ * @apiSuccess {Date} createdAt creation date
+ * @apiSuccess {String} name name of the task
+ * @apiSuccess {Date} dueDate due date of the task
+ * @apiSuccess {Number} priority task priority
+ * @apiSuccess {Number} __v version of the document
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *  {
+ *      "_id": "570bfd4ffcc31d60194e38bd",
+ *      "updatedAt": "2016-04-11T19:38:55.901Z",
+ *      "createdAt": "2016-04-11T19:38:55.901Z",
+ *      "name": "ewrwrwrwerwre",
+ *      "dueDate": "2016-04-12T05:00:00.000Z",
+ *      "priority": 4,
+ *      "__v": 0
+ *  }
+ *
+ *
+ * @apiError (404) UserNotFound The id of the User was not found.
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *          "status": 404,
+ *          "message": "Object not found",
+ *          "name": "ObjectNotFound",
+ *          "errors": {
+ *              "_id": {
+ *                  "message": "Could not find object with specified attributes"
+ *              }
+ *          }
+ *       }
+ */
+
+/**
  * @api {post} /task Creates a task
  * @apiSampleRequest /api/task
  * @apiName PostTask
@@ -75,6 +117,79 @@ var Task = require('./models/task');
  *
  * @apiError UserNotFound The id of the User was not found.
  *
+ */
+
+/**
+ * @api {put} /task/:id Update Task information
+ * @apiSampleRequest /api/task/570bfd4ffcc31d60194e38bd
+ * @apiName PutTask
+ * @apiGroup Task
+ *
+ * @apiParam {String} name name of the task
+ * @apiParam {Date} dueDate  Task due date
+ * @apiParam {Number} priority Task priority
+ *
+ * @apiSuccess {String} _id ID of the task.
+ * @apiSuccess {Date} updatedAt updated date
+ * @apiSuccess {Date} createdAt creation date
+ * @apiSuccess {String} name name of the task
+ * @apiSuccess {Date} dueDate due date of the task
+ * @apiSuccess {Number} priority task priority
+ * @apiSuccess {Number} __v version of the document
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *  {
+ *      "_id": "570bfd4ffcc31d60194e38bd",
+ *      "updatedAt": "2016-04-11T19:38:55.901Z",
+ *      "createdAt": "2016-04-11T19:38:55.901Z",
+ *      "name": "ewrwrwrwerwre",
+ *      "dueDate": "2016-04-12T05:00:00.000Z",
+ *      "priority": 4,
+ *      "__v": 0
+ *  }
+ *
+ *
+ * @apiError (404) TaskNotFound The id of the User was not found.
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *          "status": 404,
+ *          "message": "Object not found",
+ *          "name": "ObjectNotFound",
+ *          "errors": {
+ *              "_id": {
+ *                  "message": "Could not find object with specified attributes"
+ *              }
+ *          }
+ *       }
+ */
+
+/**
+ * @api {delete} /task/:id Delete Task
+ * @apiSampleRequest /api/task/570bfd4ffcc31d60194e38bd
+ * @apiName DeleteTask
+ * @apiGroup Task
+ *
+ *
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *
+ *
+ * @apiError (404) TaskNotFound The id of the User was not found.
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *          "status": 404,
+ *          "message": "Object not found",
+ *          "name": "ObjectNotFound",
+ *          "errors": {
+ *              "_id": {
+ *                  "message": "Could not find object with specified attributes"
+ *              }
+ *          }
+ *       }
  */
 
 Task.methods(['get','put','post','delete']);
